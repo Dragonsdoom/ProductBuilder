74ce03b37880e1780d55394e8613a8116fde6193
Frank V <frank@protodragon.com>
2013-09-17 07:14:08 -0400

    added many unused widgets to ui

--- inventoryItemView.py
+++ inventoryItemView.py
@@ -1,4 +1,5 @@
 import Tkinter as tk
+import ttk
 
 class invItemView():
     root = tk.Tk()
@@ -8,15 +9,45 @@ class invItemView():
         self.model = model
         model.addSubscriber(self)
 
-        self.iItemNameLabelFrame = tk.LabelFrame(self.root,text="Gun Name")
-        self.iItemNameLabelFrame.pack()
+        self.root.resizable(0,0)
+        
+        self.iItemNameLabelFrame = ttk.LabelFrame(self.root,text="Gun Name")
+        self.iItemNameLabelFrame.grid(row=0, column=1, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
 
-        self.iItemTypeLabelFrame = tk.LabelFrame(self.root,text="Product")
-        self.iItemTypeLabelFrame.pack()
+        self.iItemPhotoImageLabel = ttk.Label(self.iItemNameLabelFrame,text='insertImage')
+        self.iItemPhotoImageLabel.grid(row=0, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
+
+        self.iItemTypeLabelFrame = ttk.LabelFrame(self.root,text="Product")
+        self.iItemTypeLabelFrame.grid(row=0, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
 
         typeOption = tk.StringVar(self.root)
-        self.iItemTypeOptionMenu = tk.OptionMenu(self.iItemTypeLabelFrame,typeOption, 'iItem','product','gun')
-        self.iItemTypeOptionMenu.pack()
+        self.iItemTypeOptionMenu = ttk.OptionMenu(self.iItemTypeLabelFrame,typeOption, 'iItem','product','gun')
+        self.iItemTypeOptionMenu.pack(expand=1,fill=tk.BOTH)
+
+        self.iItemCreatedLabelFrame = ttk.LabelFrame(self.root,text='Inventory Items Created')
+        self.iItemCreatedLabelFrame.grid(row=1, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
+
+        self.iItemCreatedListBox = tk.Listbox(self.iItemCreatedLabelFrame)
+        self.iItemCreatedListBox.grid(row=0, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
+        self.iItemCreatedListBox.insert(tk.END,'Create a new item')
+
+        self.iItemAddEntry = ttk.Entry(self.iItemCreatedLabelFrame)
+        self.iItemAddEntry.grid(row=2, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
+
+        self.iItemAddButton = ttk.Button(self.iItemCreatedLabelFrame,text='Add', command=None)
+        self.iItemAddButton.grid(row=3, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
+
+        self.iItemDeleteButton = ttk.Button(self.iItemCreatedLabelFrame,text='Delete', command=None)
+        self.iItemDeleteButton.grid(row=4, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
+
+        self.iItemLoadButton = ttk.Button(self.iItemCreatedLabelFrame,text='Load', command=None)
+        self.iItemAddButton.grid(row=5, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
+
+        self.iItemSaveButton = ttk.Button(self.iItemCreatedLabelFrame,text='Save', command=None)
+        self.iItemDeleteButton.grid(row=6, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
+
+        self.iItemClearButton = ttk.Button(self.iItemCreatedLabelFrame,text='Clear', command=None)
+        self.iItemClearButton.grid(row=7, column=0, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
         
     def mainLoop(self):
         tk.mainloop()
